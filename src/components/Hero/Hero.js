e4fc7dd142670a8f50af8c08144a00e12bbd1c17
Mahmudul Hasan Sagar <36738052+mahmudsagar@users.noreply.github.com>
2024-12-22 13:12:43 +0600

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,10 +8,10 @@ const Hero = (props) => (
   <Section row nopaddin>
     <LeftSection>
       <SectionTitle main center>
-        Hello There, I'm Sagar<br/>A Web Developer
+        Hello There, I'm Sagar<br/>A Fullstack Javascript Developer
       </SectionTitle>
       <SectionText>
-        Welcome to my portfolio. I'm a web eveloper with speciality in Front End development. I can make you interactive front end design with HTML,CSS, Javascript, React etc.
+        Welcome to my portfolio. This is where passion meets code, and every project tells a story of creativity and problem-solving. Whether you’re here to explore my work, collaborate on exciting ideas, or just say hello, you’re in the right place.
       </SectionText>
       <Link href="#about">
         <Button>Learn More</Button>
